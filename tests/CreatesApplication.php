fde88206f91d97b1728a8489f1fa7579f1178cbd
Steve McDougall <juststevemcd@gmail.com>
2023-11-12 21:59:58 +0000

    Setting up testing and CI actions

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -9,13 +9,9 @@ use Illuminate\Foundation\Application;
 
 trait CreatesApplication
 {
-    /**
-     * Creates the application.
-     */
     public function createApplication(): Application
     {
         $app = require __DIR__ . '/../bootstrap/app.php';
-
         $app->make(Kernel::class)->bootstrap();
 
         return $app;
