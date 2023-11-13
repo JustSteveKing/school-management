9457aa22f9021f937cc6f33683be47db25f3966c
Steve McDougall <juststevemcd@gmail.com>
2023-11-13 11:01:51 +0000

    Adding a default user in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,21 +4,17 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 final class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'first_name' => 'Steve',
+            'last_name' => 'McDougall',
+            'email' => 'juststevemcd@gmail.com',
+        ]);
     }
 }
